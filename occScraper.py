86891457f3eaca5ac7875b2ef913aa96e0f405fc
Saul Gallegos <saulladrondeguevara@gmail.com>
2023-12-18 13:36:50 -0600

    feat: multiple base urls added, logs validated

--- occScraper.py
+++ occScraper.py
@@ -8,7 +8,20 @@ import json
 import re
 import time
 
-BASE_URL = "https://www.occ.com.mx/empleos/en-jalisco/en-la-ciudad-de-guadalajara/"
+
+BASE_URLS = [
+    "https://www.occ.com.mx/empleos/en-jalisco/en-la-ciudad-de-guadalajara/",
+    "https://www.occ.com.mx/empleos/en-ciudad-de-mexico/",
+    "https://www.occ.com.mx/empleos/en-aguascalientes/",
+    "https://www.occ.com.mx/empleos/en-nuevo-leon/en-la-ciudad-de-monterrey/",
+    "https://www.occ.com.mx/empleos/en-queretaro/",
+    "https://www.occ.com.mx/empleos/en-puebla/",
+    "https://www.occ.com.mx/empleos/en-morelos/",
+    "https://www.occ.com.mx/empleos/en-sonora/",
+    "https://www.occ.com.mx/empleos/en-chihuahua/",
+    "https://www.occ.com.mx/empleos/en-coahuila/",
+]
+NUMBER_OF_PAGES = 15
 
 
 def initialized_driver():
@@ -66,7 +79,7 @@ def get_pdf_from_url(driver, url):
 class OccScraper:
     def __init__(self):
         self.driver = initialized_driver()
-        self.driver.get(BASE_URL)
+        self.driver.get(BASE_URLS[0])
 
     def save_pdf(self, pdf, id):
         file_path = f'datasets/occ/pdf/{id}.pdf'
@@ -74,30 +87,44 @@ class OccScraper:
             file.write(base64.b64decode(pdf['data']))
         return file_path
 
-    def get_jobs_url_by_page(self, page):
-        self.driver.get(BASE_URL + f'?page={page}')
-        wait_load = WebDriverWait(self.driver, 1)
+    def get_jobs_url_by_page(self, base_index, page):
+        self.driver.get(BASE_URLS[base_index] + f'?page={page}')
+        wait_load = WebDriverWait(self.driver, 0.5)
         jobs_list = wait_load.until(
             lambda driver: driver.find_elements(By.XPATH, "//div[@class='card-0-2-520 flat-0-2-522 card-0-2-558']"))
         jobs_list = [job.find_element(
             By.XPATH, ".//a").get_attribute('href') for job in jobs_list]
-        print(f'Page {page} has {len(jobs_list)} jobs')
-        print(f'Jobs: {jobs_list}')
-        return jobs_list
+        print(
+            f'Page {page} has {len(jobs_list)} jobs. Location: {BASE_URLS[base_index]}')
 
-    def save_all_jobs_url(self):
-        jobs_list = []
-        for page in range(1, 180):
-            jobs_list += self.get_jobs_url_by_page(page)
+        current_jobs = json.load(open('occ_jobs_url.json', 'r'))
+        jobs_list += current_jobs
+        jobs_list = list(dict.fromkeys(jobs_list))
+
+        print(f'Total jobs: {len(jobs_list)}')
 
         with open('occ_jobs_url.json', 'w') as json_file:
             json.dump(jobs_list, json_file)
-        return 'occ_jobs_url.json'
+
+    def get_jobs_url(self, base_index):
+        for page in range(1, NUMBER_OF_PAGES + 1):
+            self.get_jobs_url_by_page(base_index, page)
+
+    def save_all_jobs_url(self):
+        for base_index in range(len(BASE_URLS)):
+            self.get_jobs_url(base_index)
 
     def insert_log(self, job_log_info):
         with open('logs.csv', 'a') as file:
             file.write(
                 f'{job_log_info["id"]}, {job_log_info["platform"]}, {job_log_info["date"]}, {job_log_info["txt_path"]}, {job_log_info["html_path"]}, {job_log_info["pdf_path"]}\n')
+
+    def is_job_in_log(self, job_id):
+        with open('logs.csv', 'r') as file:
+            for line in file:
+                if job_id in line:
+                    return True
+        return False
 
     def get_all_jobs_information(self, path):
         jobs_list = []
@@ -120,23 +147,25 @@ class OccScraper:
             else:
                 print("Pattern not found")
 
-            html = self.driver.page_source
-            html_path = f'datasets/occ/html/{job_log_info["id"]}.html'
-            with open(html_path, 'w') as html_file:
-                job_log_info['html_path'] = html_path
-                html_file.write(html)
+            if not self.is_job_in_log(job_log_info['id']):
+                html = self.driver.page_source
+                html_path = f'datasets/occ/html/{job_log_info["id"]}.html'
+                with open(html_path, 'w') as html_file:
+                    job_log_info['html_path'] = html_path
+                    html_file.write(html)
 
-            txt_path = f'datasets/occ/text/{job_log_info["id"]}.txt'
-            with open(txt_path, 'w') as txt_file:
-                job_log_info['txt_path'] = txt_path
-                txt_file.write(title + '\n')
-                txt_file.write(description)
+                txt_path = f'datasets/occ/text/{job_log_info["id"]}.txt'
+                with open(txt_path, 'w') as txt_file:
+                    job_log_info['txt_path'] = txt_path
+                    txt_file.write(title + '\n')
+                    txt_file.write(description)
 
-            pdf = get_pdf_from_url(self.driver, job_url)
-            pdf_path = self.save_pdf(pdf, job_log_info['id'])
-            job_log_info['pdf_path'] = pdf_path
+                pdf = get_pdf_from_url(self.driver, job_url)
+                pdf_path = self.save_pdf(pdf, job_log_info['id'])
+                job_log_info['pdf_path'] = pdf_path
 
-            self.insert_log(job_log_info)
+                self.insert_log(job_log_info)
+                print(f'Saved job: {job_log_info["id"]}')
 
     def close_driver(self):
         self.driver.close()
